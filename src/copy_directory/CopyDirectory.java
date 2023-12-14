74d98de9ff61707fbb14b16c679b0cf3e968ffee
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-12-14 15:15:12 -0300

    Update Javadocs

--- src/copy_directory/CopyDirectory.java
+++ src/copy_directory/CopyDirectory.java
@@ -17,86 +17,89 @@ import javax.swing.JOptionPane;
  * CopyDirectory
  * </h1>
  * 
- * 
  * <p>
- * The {@code CopyDirectory} is the main class of this program. This program
- * question the user in your init with two
- * {@link JOptionPane#showInputDialog(java.awt.Component, Object, String, int)}
- * that resquest the values of the {@code copyString} and {@code pasteString}
- * and his respectives paths ({@code copyPath} and {@code pastePath}). Then it
- * will create a {@link StringBuilder} named {@code sbLog} that receive all logs
- * of this copy, like the init time, all files copied and pasted and, the end
- * time.
+ * The {@code CopyDirectory} is the main class of this program. In its
+ * execution, it interacts with the user through {@link JOptionPane} panels with
+ * the objective of copy and paste files/paths.
  * </p>
  * 
  * <p>
- * To prevent an infinite recursion error, caused when the {@code pastePath} is
- * inside the {@code copyPath}, this program then checks for the existence of
- * this possible error in a {@code try block}. First is stored in two
- * {@link String} objects the absolute path of {@code copyPath} and
- * {@code pastePath}. Then verify if {@code absPastePath} contains
- * {@code absCopyPath} with the method {@link String#contains(CharSequence)}.
- * If it is true, is throwed a new
- * {@link InputMismatchException#InputMismatchException(String)}, that receive
- * in the {@link String} parameter a detail message that explains the error.
- * Then, in the {@code catch} block, is showed a
- * {@link JOptionPane#showMessageDialog(java.awt.Component, Object, String, int)}
- * that warns of the error and terminate the program with a
- * {@link System#exit(int)} as parameter {@code -1} to indicates unsuccessful
- * termination with Exception.
+ * With special emphasis, much of the knowledge used here was taken from chapter
+ * 2 of the book Core Java II (tenth edition) by Cay S. Horstmann.
  * </p>
- * 
- * <p>
- * With the {@link Files#walk(Path, java.nio.file.FileVisitOption...)} that
- * returs a {@link Stream} of {@link Path}, this program will try traverse this
- * {@code Stream<Path>} with a {@link Stream#forEach(Consumer)}. As this
- * {@link Consumer} is passed a {@code lambda} with the parameter {@code p} of
- * type {@link Path} and int the body the process of copy and paste.
- * </p>
- * 
- * <p>
- * Into the body of the consumer, the program will try create a {@link Path}
- * named {@code q} that receive the
- * {@code pastePath.resolve(copyPath.relativize(p))}. Then print what file is
- * being copied and what file is being pasted. Next, test with the
- * {@link Files#isDirectory(Path, java.nio.file.LinkOption...)} if {@code p}
- * (the path being copied) is a directory, if the test is true, is created the
- * directory {@code q} (the path being pasted) with the method
- * {@link Files#createDirectory(Path, java.nio.file.attribute.FileAttribute...)};
- * else the file on the path {@code p} is copied to the path {@code q} with the
- * method {@link Files#copy(Path, Path, java.nio.file.CopyOption...)}.
- * </p>
- * 
- * <p>
- * After the scope of the {@code try} that contains the
- * {@link Files#walk(Path, java.nio.file.FileVisitOption...)} is terminated and
- * processed the log, finally it is writed.
- * </p>
- * 
- * <p>
- * Finally is showed in a
- * {@link JOptionPane#showConfirmDialog(java.awt.Component, Object, String, int)}
- * a message notifying the finish of the copy from {@code copyPath} to
- * {@code pasteString}.
- * </p>
- * 
- * 
- * 
- * @implSpec
- *           <h2>
- *           Warning:
- *           </h2>
- * 
- *           <p>
- *           A bug occurs when is called from copy one directory to another
- *           inside the copied directory, causing recursivity. In the model:
- *           copy {@code path/src/} to {@code path/src/test}
- *           </p>
  * 
  * @author joaoGHF
  * @version 1.0, 13/12/2023
  */
 public class CopyDirectory {
+    /**
+     * <h1>
+     * Main Method
+     * </h1>
+     * 
+     * <p>
+     * This program question the user in your init with two
+     * {@link JOptionPane#showInputDialog(java.awt.Component, Object, String, int)}
+     * that resquest the values of the {@code copyString} and {@code pasteString}
+     * and his respectives paths ({@code copyPath} and {@code pastePath}). Then it
+     * will create a {@link StringBuilder} named {@code sbLog} that receive all logs
+     * of this copy, like the init time, all files copied and pasted and, the end
+     * time.
+     * </p>
+     * 
+     * <p>
+     * To prevent an infinite recursion error, caused when the {@code pastePath} is
+     * inside the {@code copyPath}, this program then checks for the existence of
+     * this possible error in a {@code try block}. First is stored in two
+     * {@link String} objects the absolute path of {@code copyPath} and
+     * {@code pastePath}. Then verify if {@code absPastePath} contains
+     * {@code absCopyPath} with the method {@link String#contains(CharSequence)}.
+     * If it is true, is throwed a new
+     * {@link InputMismatchException#InputMismatchException(String)}, that receive
+     * in the {@link String} parameter a detail message that explains the error.
+     * Then, in the {@code catch} block, is showed a
+     * {@link JOptionPane#showMessageDialog(java.awt.Component, Object, String, int)}
+     * that warns of the error and terminate the program with a
+     * {@link System#exit(int)} as parameter {@code -1} to indicates unsuccessful
+     * termination with Exception.
+     * </p>
+     * 
+     * <p>
+     * With the {@link Files#walk(Path, java.nio.file.FileVisitOption...)} that
+     * returs a {@link Stream} of {@link Path}, this program will try traverse this
+     * {@code Stream<Path>} with a {@link Stream#forEach(Consumer)}. As this
+     * {@link Consumer} is passed a {@code lambda} with the parameter {@code p} of
+     * type {@link Path} and int the body the process of copy and paste.
+     * </p>
+     * 
+     * <p>
+     * Into the body of the consumer, the program will try create a {@link Path}
+     * named {@code q} that receive the
+     * {@code pastePath.resolve(copyPath.relativize(p))}. Then print what file is
+     * being copied and what file is being pasted. Next, test with the
+     * {@link Files#isDirectory(Path, java.nio.file.LinkOption...)} if {@code p}
+     * (the path being copied) is a directory, if the test is true, is created the
+     * directory {@code q} (the path being pasted) with the method
+     * {@link Files#createDirectory(Path, java.nio.file.attribute.FileAttribute...)};
+     * else the file on the path {@code p} is copied to the path {@code q} with the
+     * method {@link Files#copy(Path, Path, java.nio.file.CopyOption...)}.
+     * </p>
+     * 
+     * <p>
+     * After the scope of the {@code try} that contains the
+     * {@link Files#walk(Path, java.nio.file.FileVisitOption...)} is terminated and
+     * processed the log, finally it is writed.
+     * </p>
+     * 
+     * <p>
+     * Finally is showed in a
+     * {@link JOptionPane#showConfirmDialog(java.awt.Component, Object, String, int)}
+     * a message notifying the finish of the copy from {@code copyPath} to
+     * {@code pasteString}.
+     * </p>
+     * 
+     * @param args aren't used in this program.
+     */
     public static void main(String[] args) {
         String copyString = JOptionPane.showInputDialog(null, "Enter the diretory to copy:", "CopyDirectory",
                 JOptionPane.QUESTION_MESSAGE);
